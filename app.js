815f8a8a1e746119f258b41773bb16b96e32f13a
opetaja <opetaja@tpt.lan>
2025-04-04 13:35:49 +0300

    Add Osama

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
     { name: 'milkshake', img: 'images/milkshake.png' },
     { name: 'hotdog', img: 'images/hotdog.png' },
     { name: 'fries', img: 'images/fries.png' },
+    { name: 'osama', img: 'images/download.png' },
     { name: 'cheeseburger', img: 'images/cheeseburger.png' },
     { name: 'ice-cream', img: 'images/ice-cream.png' },
     { name: 'pizza', img: 'images/pizza.png' },
     { name: 'milkshake', img: 'images/milkshake.png' },
-    { name: 'hotdog', img: 'images/hotdog.png' }
+    { name: 'hotdog', img: 'images/hotdog.png' },
+    { name: 'osama', img: 'images/download.png' }
   ];
 
   const grid = document.querySelector('.grid');
